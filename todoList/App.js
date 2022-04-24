a10224318b1ab9222bbf355acb03721026eeb603
Tomas Farias <85124666+TomasFarias11@users.noreply.github.com>
2022-04-23 23:23:18 -0300

    Update App.js

--- todoList/App.js
+++ todoList/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navigation from './Navigation.js'
 import {Provider} from 'react-redux'
-import store from './store/index.js'
+import store from './src/store/index.js'
 
 
 export default function App () {
